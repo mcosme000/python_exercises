d63ba9021b547ad2011bc6ea2484fbf7eda786d9
Maria Cosmen <mcosme00@gmail.com>
2023-07-20 17:11:07 +0900

    Challenge refactoring

--- 100_days_of_python/005.password_generator.py
+++ 100_days_of_python/005.password_generator.py
@@ -15,25 +15,19 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-randomLetters = []
+password = ""
 for number in range(0, nr_letters):
-    rand = letters[random.randint(0, len(letters) - 1)]
-    randomLetters.append(rand)
-print(randomLetters)
+    password += random.choice(letters)
 
-randomSymbols = []
+randomSymbols = ""
 for number in range(0, nr_symbols):
-    rand = symbols[random.randint(0, len(symbols) - 1)]
-    randomSymbols.append(rand)
-print(randomSymbols)
+    password += random.choice(symbols)
 
-randomNumbers = []
+randomNumbers = ""
 for number in range(0, nr_numbers):
-    rand = numbers[random.randint(0, len(numbers) - 1)]
-    randomNumbers.append(rand)
-print(randomNumbers)
+    password += random.choice(numbers)
 
-password = f"{''.join(randomLetters)}{''.join(randomSymbols)}{''.join(randomNumbers)}"
+
 print(f"My password: {password}")
 
 
@@ -41,3 +35,5 @@ print(f"My password: {password}")
 pass_arr = [*password]
 random.shuffle(pass_arr)
 print(f"My random password: {''.join(pass_arr)}")
+
+print([*"hi"])
