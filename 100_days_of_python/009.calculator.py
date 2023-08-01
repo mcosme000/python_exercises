66c169061783778f13f74da093fa9fd653835ee0
Maria Cosmen <mcosme00@gmail.com>
2023-08-01 10:30:02 +0900

    Calculator challenge

--- 100_days_of_python/009.calculator.py
+++ 100_days_of_python/009.calculator.py
@@ -22,16 +22,32 @@ operations = {
     "/": divide
 }
 
-is_calculating = True
-while is_calculating:
-    number1 = int(input("First number \n> "))
-    number2 = int(input("Second number \n> "))
-    op = input("What operation do you want to do? [ + | - | * | / ]}\n> ")
+def calculator():
+    print(calculator_art.logo)
+    number1 = float(input("Pick a number: \n> "))
+    is_calculating = True
 
-    while not op in operations:
-        print("Wrong command, please chose one of the following: [ + | - | * | / ] ")
-        op = input("What operation do you want to do? [ + | - | * | / ]}\n> ")
+    while is_calculating:
+        op = input("Pick an operation [ + | - | * | / ]}\n> ")
+        number2 = float(input("What is the next number? \n> "))
 
-    print(f"The result is {operations[op](number1, number2)}")
-    choice = input("Do you want to use the calculator again? [ YES | NO ]\n ").lower()
-    if not choice == "yes": is_calculating = False
+        while not op in operations:
+            print("Wrong command, please chose one of the following: [ + | - | * | / ] ")
+            op = input("What operation do you want to do? [ + | - | * | / ]}\n> ")
+
+        result = operations[op](number1, number2)
+        print(f"{number1} {op} {number2} = {result}")
+        print("What do you want to do next?")
+        print(f"[ 1 ] Continue calculating with {result}")
+        print("[ 2 ] Calculate with two other numbers")
+        print("[ 3 ] Exit calculator")
+        choice = int(input("> "))
+        match choice:
+            case 1:
+                number1 = result
+            case 2:
+                is_calculating = False
+                calculator()
+            case 3: is_calculating = False
+
+calculator()
