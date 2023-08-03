8a5779859fc11a7217050efe0ef4b2967ea7ab0c
Maria Cosmen <mcosme00@gmail.com>
2023-08-03 15:29:30 +0900

    Debug challenge

--- 100_days_of_python/012.higher_lower.py
+++ 100_days_of_python/012.higher_lower.py
@@ -1,6 +1,45 @@
 import random
 
 
+import higher_lower_data
+score = 0
+game = True
+a = random.choice(higher_lower_data.data)
+
+def compare(guess):
+    global a, b
+    if guess == "a":
+        return a['follower_count'] > b['follower_count']
+    else:
+        if b['follower_count'] > a['follower_count']:
+            a = b
+            return True
+
+
+def check_answer(guess):
+    global score, game
+    answer = compare(guess)
+    if answer:
+        score += 1
+        print(f"Score: {score}")
+    else:
+        print("We are sorry, you lost")
+        print(f"You scored {score} points!")
+        game = False
+
+
+while game:
+    b = random.choice(higher_lower_data.data)
+    if a == b: b = random.choice(higher_lower_data.data)
+
+    print(f"*** DEBUG: {a['name']}: {a['follower_count']} VS {b['name']}: {b['follower_count']}")
+    print(f"Compare A: {a['name']}")
+    print("VS")
+    print(f"Agains B: {b['name']}")
+
+    guess = input("Who has more followers? 'A' or 'B': ").lower()
+    check_answer(guess)
+
 
 """
 - - - PSEUDO CODE - - -
@@ -15,41 +54,3 @@ import random
    if False:
         - stop game
 """
-
-import higher_lower_data
-score = 0
-game = True
-a = random.choice(higher_lower_data.data)
-
-def compare(guess):
-    global a, b
-    if guess == "a":
-        a = a
-        return a['follower_count'] > b['follower_count']
-    else:
-        a = b
-        return b['follower_count'] > a['follower_count']
-
-
-def check_answer(guess):
-    global score
-    global game
-    answer = compare(guess)
-    if answer:
-        score += 1
-        print(f"Score: {score}")
-    else:
-        print("We are sorry, you lost")
-        print(f"You scored {score} points!")
-        game = False
-
-
-while game:
-    b = random.choice(higher_lower_data.data)
-    # print(f"*** DEBUG: {a['name']}: {a['follower_count']} VS {b['name']}: {b['follower_count']}")
-    print(f"Compare A: {a['name']}")
-    print("VS")
-    print(f"Agains B: {b['name']}")
-
-    guess = input("Who has more followers? 'A' or 'B': ").lower()
-    check_answer(guess)
